3beecad4c98b94efd2a59c3decd01e827aba7000
Traun Leyden <traun@couchbase.com>
2013-06-30 23:15:58 -0700

    function to print topology overview for debugging

--- util.go
+++ util.go
@@ -1,6 +1,7 @@
 package neurvolve
 
 import (
+	"fmt"
 	ng "github.com/tleyden/neurgo"
 	"math"
 )
@@ -11,4 +12,12 @@ func randomWeights(length int) []float64 {
 		weights = append(weights, ng.RandomInRange(-1*math.Pi, math.Pi))
 	}
 	return weights
+}
+
+func printTopology(neuralNet *ng.NeuralNetwork) {
+	nodesByLayer := neuralNet.NodesByLayer()
+	for _, nodesInLayer := range nodesByLayer {
+		fmt.Printf("[%d]-", len(nodesInLayer))
+	}
+	fmt.Printf("|\n")
 }
