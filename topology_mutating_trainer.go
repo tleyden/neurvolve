3489c902f43bce71053d5ba4988e406bca390305
Traun Leyden <traun@couchbase.com>
2013-10-26 16:33:50 -0700

    refactor to use training example scape.

--- topology_mutating_trainer.go
+++ topology_mutating_trainer.go
@@ -108,92 +108,9 @@ func (tmt *TopologyMutatingTrainer) Train(cortex *ng.Cortex, scape Scape) (fitte
 
 func (tmt *TopologyMutatingTrainer) TrainExamples(cortex *ng.Cortex, examples []*ng.TrainingSample) (fittestCortex *ng.Cortex, succeeded bool) {
 
-	ng.SeedRandom()
-
-	includeNonTopological := false
-	mutators := CortexMutatorsNonRecurrent(includeNonTopological)
-
-	originalCortex := cortex.Copy()
-
-	currentCortex := cortex
-	currentCortex.RenderSVGFile("/Users/traun/tmp/current.svg")
-
-	// Apply NN to problem and save fitness
-	fitness := currentCortex.Fitness(examples)
-
-	if fitness > tmt.FitnessThreshold {
-		succeeded = true
-		return
+	trainingSampleScape := &TrainingSampleScape{
+		examples: examples,
 	}
-
-	for i := 0; ; i++ {
-
-		log.Printf("before mutate.  i/max: %d/%d", i, tmt.MaxAttempts)
-
-		// before we mutate the cortex, we need to init it,
-		// otherwise things like Outsplice will fail because
-		// there are no DataChan's.
-		currentCortex.Init()
-
-		// mutate the network
-		randInt := RandomIntInRange(0, len(mutators))
-		mutator := mutators[randInt]
-		ok, _ := mutator(currentCortex)
-		if !ok {
-			log.Printf("mutate didn't work, retrying...")
-			continue
-		}
-
-		isValid := currentCortex.Validate()
-		if !isValid {
-			log.Panicf("Cortex did not validate")
-		}
-
-		filenameJson := fmt.Sprintf("cortex-%v.json", i)
-		currentCortex.MarshalJSONToFile(filenameJson)
-		filenameSvg := fmt.Sprintf("cortex-%v.svg", i)
-		currentCortex.RenderSVGFile(filenameSvg)
-		log.Printf("after mutate. cortex written to: %v and %v", filenameSvg, filenameJson)
-
-		log.Printf("run stochastic hill climber")
-
-		// memetic step: call stochastic hill climber and see if it can solve it
-		shc := &StochasticHillClimber{
-			FitnessThreshold:           ng.FITNESS_THRESHOLD,
-			MaxIterationsBeforeRestart: 20000,
-			MaxAttempts:                10,
-			WeightSaturationRange:      tmt.WeightSaturationRange,
-		}
-		fittestCortex, succeeded = shc.TrainExamples(currentCortex, examples)
-		log.Printf("stochastic hill climber finished.  succeeded: %v", succeeded)
-
-		if succeeded {
-			succeeded = true
-			break
-		}
-
-		if i >= tmt.MaxAttempts {
-			succeeded = false
-			break
-		}
-
-		if ng.IntModuloProper(i, tmt.MaxIterationsBeforeRestart) {
-			log.Printf("** restart.  i/max: %d/%d", i, tmt.MaxAttempts)
-
-			currentCortex = originalCortex.Copy()
-			isValid := currentCortex.Validate()
-			if !isValid {
-				currentCortex.Repair() // TODO: remove workaround
-				isValid = currentCortex.Validate()
-				if !isValid {
-					log.Panicf("Cortex could not be repaired")
-				}
-			}
-
-		}
-
-	}
-
-	return
+	return tmt.Train(cortex, trainingSampleScape)
 
 }
