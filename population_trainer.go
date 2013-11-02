57e7e14f58a4dc0395804d4a3d453810316ed5c4
Traun Leyden <traun@couchbase.com>
2013-11-02 11:40:30 -0700

    stub out more of the population trainer

--- population_trainer.go
+++ population_trainer.go
@@ -17,11 +17,29 @@ type FitCortex struct {
 
 func (pt *PopulationTrainer) Train(population []*ng.Cortex, scape ScapeTwoPlayer) (population []FitCortex, succeeded bool) {
 
+	for i := 0; i < pt.MaxGenerations; i++ {
+
+		fitCortexes := pt.computeFitness(population, scape)
+
+		if pt.exceededFitnessThreshold(fitCortexes) {
+			succeeded = true
+			population = fitCortexes
+			return
+		}
+
+		fitCortexes = pt.cullPopulation(fitCortexes)
+		fitCortexes = pt.generateOffspring(fitCortexes)
+
+	}
+
+	population = fitCortexes
+	return
+
 }
 
-func (pt *PopulationTrainer) computeFitness(population []*ng.Cortex, scape ScapeTwoPlayer) (population []FitCortex) {
+func (pt *PopulationTrainer) computeFitness(population []*ng.Cortex, scape ScapeTwoPlayer) (fitCortexes []FitCortex) {
 
-	fitCortexes := make([]FitCortex, len(population))
+	fitCortexes = make([]FitCortex, len(population))
 	for i, cortex := range population {
 		opponents := pt.chooseRandomOpponents(population, 5)
 		fitnessScores := make([]float64, len(opponents))
@@ -33,6 +51,41 @@ func (pt *PopulationTrainer) computeFitness(population []*ng.Cortex, scape Scape
 			Fitness: pt.calculateAverage(fitnessScores),
 		}
 		fitCortexes[i] = fitCortex
+	}
+
+	fitCortexes = pt.sortByFitness(fitCortexes)
+
+	return
+}
+
+func (pt *PopulationTrainer) exceededFitnessThreshold(fitCortexes []FitCortex) bool {
+
+}
+
+func (pt *PopulationTrainer) cullPopulation(population []FitCortex) (culledPopulation []FitCortex) {
+
+}
+
+func (pt *PopulationTrainer) generateOffspring(population []FitCortex) (withOffspring []FitCortex) {
+
+	withOffspring = make([]FitCortex, 2*len(population))
+	withOffspring = append(withOffspring, population...)
+
+	for i, fitCortex := range population {
+
+		cortex := fitCortex.Cortex
+		offspringCortex := cortex.Copy()
+		succeeded, _ := pt.CortexMutator(offspringCortex)
+		if !succeeded {
+			logg.LogPanic("Unable to mutate cortex: %v", offspringCortex)
+		}
+		fitCortexOffspring := FitCortex{
+			Cortex:  offspringCortex,
+			Fitness: 0.0,
+		}
+
+		withOffspring = append(withOffspring, fitCortexOffspring)
+
 	}
 
 }
