197a8de6fe021fdb2562c4ac2a6b065c0b711c45
Traun Leyden <traun@couchbase.com>
2013-11-11 20:19:53 -0800

    refactor saturate to its own method, call it from perturbParameterBellCurve

--- math_util.go
+++ math_util.go
@@ -10,7 +10,12 @@ const DEFAULT_STD_DEVIATION = 1.5
 
 func perturbParameter(parameter float64, saturationBounds []float64) float64 {
 
-	parameter += ng.RandomInRange(-1*math.Pi, math.Pi)
+	parameter += ng.RandomInRange(-2*math.Pi, 2*math.Pi)
+	return saturate(parameter, saturationBounds)
+
+}
+
+func saturate(parameter float64, saturationBounds []float64) float64 {
 
 	lowerBound := saturationBounds[0]
 	upperBound := saturationBounds[1]
@@ -28,6 +33,10 @@ func perturbParameter(parameter float64, saturationBounds []float64) float64 {
 func perturbParameterBellCurve(parameter float64, desiredStdDev float64) float64 {
 
 	desiredMean := parameter
-	return rand.NormFloat64()*desiredStdDev + desiredMean
+	parameter = rand.NormFloat64()*desiredStdDev + desiredMean
+
+	saturationBounds := []float64{-10 * math.Pi, 10 * math.Pi} // todo: pass this in as a parameter
+
+	return saturate(parameter, saturationBounds)
 
 }
